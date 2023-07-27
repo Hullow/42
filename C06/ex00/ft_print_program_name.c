5bcf888f89ba68376c3c9eb21459bf2600563d63
Hullow <francisallan@gmail.com>
2023-07-28 01:07:02 +0200

    C06 up to ex03

--- C06/ex00/ft_print_program_name.c
+++ C06/ex00/ft_print_program_name.c
@@ -4,11 +4,19 @@ int	main(int argc, char **argv)
 	int	i;
 
 	i = 0;
+	
 	if (argc > 0)
 	{
 		while (argv[0][i] != 0)
 			i++;
 	write(1, argv[0], i);
+	write(1, "\n", 1);
 	}
 	return (0);
 }
+
+/* Test:
+	write(1, argv[0], i); works because argv[0] is a pointer. So it prints i characters of that pointer. 
+	write(1, &argv[0][i], 1); //argv[][] is a value, need & to reference
+	write(1, " ", 1);
+*/
---
+++ C06/ex01/ft_print_params.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	j = 0;
+
+	if (argc > 1)
+	{
+		while (argv[i] != 0)
+		{
+			j = 0;
+			while (argv[i][j] != 0)
+				j++;
+			write(1, argv[i], j);
+			write(1, "\n", 1);
+			i++;
+		}
+	}
+	return (0);
+}
