a2b001d84ea6d0c4e83d682097525f66ff4eb8f1
Francis Allan <fallan@c1r3s6.42lausanne.ch>
2023-11-13 16:12:16 +0100

    ft_strlcat, toupper, tolower

--- Libft/ft_toupper.c
+++ Libft/ft_toupper.c
@@ -6,7 +6,7 @@
 /*   By: fallan <fallan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 15:24:37 by fallan            #+#    #+#             */
-/*   Updated: 2023/10/18 15:26:44 by fallan           ###   ########.fr       */
+/*   Updated: 2023/11/13 13:36:42 by fallan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,19 @@ int	ft_toupper(int c)
 	if (c >= 97 && c <= 122)
 		return (c - 32);
 	else
-		return (1);
+		return (c);
 }
 
-/* #include <stdio.h>
+/*
+#include <stdio.h>
 #include <ctype.h>
 int	main(void)
 {
 	int	character;
 
-	character = 'i';
-
+	characters = {'a','b', 'c', 'y', '0', ' ', '9', 'z', ;
 	printf("The return value of tupper is %c\n", toupper(character));
 	printf("The return value of ft_tupper is %c\n", ft_toupper(character));
 	printf("The variable character is now equal to: %c\n", character);
-		return (0);
-} */
+	return (0);
+}*/
