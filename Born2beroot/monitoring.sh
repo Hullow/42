c12df5a1ed4774781fecdd1415a04bb827e98942
Francis Allan <fallan@c1r3s6.42lausanne.ch>
2024-01-12 22:05:17 +0100

    B2BRoot script

--- Born2beroot/monitoring.sh
+++ Born2beroot/monitoring.sh
@@ -1,19 +1,39 @@
 CPU_PHYSICAL=$(lscpu | awk '$1=="CPU(s):" {print $2}')
+VCPU=$(nproc)
 
 DISK_USED=$(df -BM --total | grep total | awk '{sub(/M$/, "", $3); print $3}')
 DISK_CAPACITY=$(df -h --total| awk '$1=="total" {print $2}')
 DISK_USAGE=$(df -hP --total| grep total | awk '{print $5}')
 
+
 MEM_TOTAL=$(free --mega| grep Mem: | awk '{print $2}')
 MEM_USED=$(free --mega| grep Mem: | awk '{print $3}')
-MEM_USED_PERCENT=$(echo "scale=2; ($MEM_USED/$MEM_TOTAL)*100" | bc)
 
-TCP_CONNECTIONS=$(awk 'NR>1 {count++} END {print count}' /proc/net/tcp)
+MEM_USED_PERCENT=$(free --mega| awk '$1=="Mem:" {printf("%.2f"), $3/$2*100}')
+#MEM_USED_PERCENT=$(echo "scale=2; ($MEM_USED/$MEM_TOTAL)*100" | bc)
 
-echo "Broadcast message from $(whoami)@$(hostname) ($(date)):"
-echo "\t#Architecture: $(uname -a)"
-echo "\t#CPU physical: $CPU_PHYSICAL"
-echo "\t#vCPU: $(nproc)"
-echo "\t#Memory Usage: $MEM_USED/$MEM_TOTAL \bMB ($MEM_USED_PERCENT%)"
-echo "\t#Disk Usage: $DISK_USED/$DISK_CAPACITY ($DISK_USAGE)"
-echo "\t#Active TCP connections: $TCP_CONNECTIONS ESTABLISHED"
+CPU_LOAD=$(mpstat | awk '$3=="all" {print 100-$13}')
+LAST_BOOT=$(who -b | awk '{print $3" "$4" "$5}')
+LVM_ACTIVE=$(if [ $(lsblk | grep "lvm" | wc -l) -eq 0 ]; then echo no; else echo yes; fi)
+TCP_CONNECTIONS=$(ss | grep "tcp" | wc -l)
+SERVER_USERS=$(users | wc -w)
+IP_ADDRESS=$(hostname -I)
+MAC_ADDRESS=$(ip -4 link show | grep ether | awk '{print $2}')
+SUDOS=$(cat /var/log/sudo/sudo.log | grep "COMMAND" | wc -l)
+SUDO=$(journalctl _COMM=sudo -q) > sudocount.tmp | awk '{while (match($0, /COMMAND/)) {count++; $0=substr($0, RSTART+RLENGTH)}} END {print count}' sudocount
+#This yields an error "-bash: Dec: command not found"
+
+wall "Broadcast message from $(whoami)@$(hostname) ($(date)):
+
+	#Architecture: $(uname -a)
+	#CPU physical: $CPU_PHYSICAL
+	#vCPU: $VCPU
+	#Memory Usage: $MEM_USED/${MEM_TOTAL}MB ($MEM_USED_PERCENT%)
+	#Disk Usage: $DISK_USED/$DISK_CAPACITY ($DISK_USAGE)
+	#CPU load: $CPU_LOAD%
+	#Last boot: $LAST_BOOT
+	#LVM active: $LVM_ACTIVE
+	#Active TCP connections: $TCP_CONNECTIONS ESTABLISHED
+	#User log: $SERVER_USERS
+	#Network: $IP_ADDRESS ($MAC_ADDRESS)
+	#Sudo: $SUDO cmd"
