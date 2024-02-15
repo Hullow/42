7b2eed28018b1838c602b88f533084b6072da9f1
Francis Allan <fallan@c1r3s6.42lausanne.ch>
2024-02-15 16:35:17 +0100

    GNL: details

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -7,7 +7,7 @@ char	*get_next_line(int fd)
 	char		*buf;
 	char		*end_of_line;
 	char		*line = "";
-	char		*temp;
+	char		*temp = NULL;
 	static char	*next_lines = "";
 	static int	test_line_check = 0;
 	int			end_reached = 0;
@@ -18,28 +18,27 @@ char	*get_next_line(int fd)
 	// if there is something left in our variable "next_lines", set temp to next_lines
 	if (ft_strlen(next_lines))
 	{
+		if (test_line_check > 3) printf("ft_strlen(next_lines) is %zu, next_lines is \"%s\" and temp is \"%s\"\n", ft_strlen(next_lines), next_lines, temp);
 		temp = next_lines;
-		// if (test_line_check > 5) printf("temp is assigned to next_lines, i.e. to \"%s\"\n", temp);
+		if (test_line_check > 3) printf("temp is now assigned to next_lines, i.e. to \"%s\"\n", temp);
 	}
 	else
 	{
-		(read(fd, (temp = buf), BUFFER_SIZE));
+		temp = ft_fill_zero(buf, BUFFER_SIZE);
+		read(fd, (temp = buf), BUFFER_SIZE);
 		// if (test_line_check > 5) printf("temp has been read into again, and is now: \"%s\"\n", temp);
 	}
-	// this while() checks for '\n' using ft_end_of_line.
+	// this while() checks for '\n' using in temp (i.e. buf or next_lines) using ft_end_of_line.
 	// If none, it adds temp's contents to our *line*, then reads more into temp
-	// if no '\n' in buf or next_lines, add contents to line and read further.
-	// Problem: infinite loop. Why ? 
-	// Take our example: 
-	while (((end_of_line = ft_end_of_line(temp)) == 0) && ft_strlen(temp)) // if no '\n' in buf or next_lines, add contents to line and read further
+	while (((end_of_line = ft_end_of_line(temp)) == 0) && ft_strlen(temp))
 	{
 		line = ft_add_string(temp, line);
-		// if (test_line_check > 5) printf("in the while loop, line has now been assigned to:\n\"%s\"\n\n", line);
+		if (test_line_check > 4) printf("in the while loop, line has now been assigned to:\n\"%s\"\n\n", line);
 		free(end_of_line);
- 		// if (test_line_check > 5) printf("   and temp is \"%s\"\n\n", temp);
+ 		if (test_line_check > 4) printf("   and temp is \"%s\"\n\n", temp);
 		temp = ft_fill_zero(buf, BUFFER_SIZE); // implicit malloc, freed by free(buf) at the end of the function
-		if (read(fd, (temp = buf), BUFFER_SIZE) < 5) end_reached = 1;
-		// if (test_line_check > 5) printf("After ft_fill_zero and reading again, temp is \"%s\" at address %p\n*********************************************************\n", temp, temp);
+		if (read(fd, (temp = buf), BUFFER_SIZE) < BUFFER_SIZE) end_reached = 1; // if read returns less than BUFFER_SIZE characters, it means it has reached end of file
+		if (test_line_check > 4) printf("After ft_fill_zero and reading again, temp is \"%s\" at address %p\n*********************************************************\n", temp, temp);
 	}
 	// if (test_line_check > 5) printf("after our while(), buf is:\n\"%s\" at address %p\n\n", buf, buf);
 	// if (test_line_check > 5) printf("temp is:\n\"%s\" at address %p\n\n", temp, temp);
@@ -47,6 +46,7 @@ char	*get_next_line(int fd)
 	// if we find \n in buf or next_lines, add contents up to '\n' to line, and after '\n' to next_lines
 	if ((end_of_line = ft_end_of_line(temp)))
 	{
+		if (test_line_check > 4) printf("We've hit end of line. temp is \"%s\" and end_of_line has been assigned to \"%s\"\n", temp, end_of_line);
 		line = ft_add_string(end_of_line, line); // add the buffer's contents to the "line" string;
 		// if (test_line_check > 5) printf("having added \"end of line\", line is:\n\"%s\"\n", line);
 		next_lines = ft_next_lines(temp);
@@ -68,23 +68,31 @@ int main(void)
 	char path[] = "/Users/fallan/42/get_next_line/example.txt";
 	int fd = open(path, O_RDONLY);
 	// printf("next line:%s\n", get_next_line(fd));
-	// printf("*********************************************************\n*********************************************************\n");
+	printf("*********************************************************\n*********************************************************\n");
 	printf("next line:%s\n", get_next_line(fd));
-	// printf("*********************************************************\n*********************************************************\n");
+	printf("*********************************************************\n*********************************************************\n");
 	printf("next line:%s\n", get_next_line(fd));
-	// printf("*********************************************************\n*********************************************************\n");
+	printf("*********************************************************\n*********************************************************\n");
 	printf("next line:%s\n", get_next_line(fd));
-	// printf("*********************************************************\n*********************************************************\n");
+	printf("*********************************************************\n*********************************************************\n");
 	printf("next line:%s\n", get_next_line(fd));
-	// printf("*********************************************************\n*********************************************************\n");
+	printf("*********************************************************\n*********************************************************\n");
 	printf("next line:%s\n", get_next_line(fd));
-	// printf("*********************************************************\n*********************************************************\n");
+	printf("*********************************************************\n*********************************************************\n");
 	printf("next line:%s\n", get_next_line(fd));
-	// printf("*********************************************************\n*********************************************************\n");
+	printf("*********************************************************\n*********************************************************\n");
 	printf("next line:%s\n", get_next_line(fd));
+	printf("*********************************************************\n*********************************************************\n");
 	printf("next line:%s\n", get_next_line(fd));
+	printf("*********************************************************\n*********************************************************\n");
 	printf("next line:%s\n", get_next_line(fd));
+	printf("*********************************************************\n*********************************************************\n");
 	printf("next line:%s\n", get_next_line(fd));
+	printf("*********************************************************\n*********************************************************\n");
+	printf("next line:%s\n", get_next_line(fd));
+	printf("*********************************************************\n*********************************************************\n");
+	printf("next line:%s\n", get_next_line(fd));
+	printf("*********************************************************\n*********************************************************\n");
 	printf("next line:%s\n", get_next_line(fd));
 	close(fd);
 	return (0);
